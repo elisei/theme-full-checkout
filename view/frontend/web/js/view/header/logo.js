05a61d9e430fbc1e012c8a9968d016efa2f9c5da
elisei <brunoelisei@o2ti.com>
2021-12-02 09:54:40 -0300

    O2TI 🖤 Magento
    Fixed #2

--- view/frontend/web/js/view/header/logo.js
+++ view/frontend/web/js/view/header/logo.js
@@ -9,31 +9,95 @@ define([
 	"ko",
 	"uiComponent",
 	"underscore",
+	"Magento_Ui/js/modal/confirm",
+	"mage/translate"
 ], function (
 	$,
 	ko,
 	Component,
 	_,
+	confirmation,
+	$t
 ) {
 	"use strict";
 	return Component.extend({
 		defaults: {
 			template: "O2TI_ThemeFullCheckout/header/logo",
 		},
+
+		/**
+		 * Src to Logo.
+		 */
 		getLogoSrc() {
 			return window.checkoutConfig.logo_src;
 		},
+
+		/**
+		 * Width to Logo.
+		 */
 		getLogoWidth() {
 			return window.checkoutConfig.logo_width;
 		},
+
+		/**
+		 * Height to Logo.
+		 */
 		getLogoHeight() {
 			return window.checkoutConfig.logo_height;
 		},
+
+		/**
+		 * Alt to Logo.
+		 */
 		getLogoAlt() {
 			return window.checkoutConfig.logo_alt;
 		},
+
+		/**
+		 * Checkout Cart Url.
+		 */
 		getLogoUrl() {
 			return window.checkoutConfig.cartUrl;
+		},
+
+		/**
+		 * Go To Cart Url.
+		 */
+		goToCartUrl() {
+			var self = this,
+				textAlert;
+
+			textAlert = $t('When you abandon checkout, you may lose your item reservation.');
+
+			confirmation({
+				title: $t('Exit Checkout?'),
+				content: textAlert,
+				actions: {
+
+					confirm: function () {
+						window.location = self.getLogoUrl()
+					},
+
+					cancel: function () {
+						return false;
+					}
+				},
+				buttons: [{
+					text: $t('Cancel'),
+					class: 'action-primary action-dismiss',
+					click: function (event) {
+						this.closeModal(event);
+					}
+				}, {
+					text: $t('Exit Checkout'),
+					class: 'action-secondary action-accept',
+					click: function (event) {
+						this.closeModal(event, true);
+						window.location = self.getLogoUrl();
+					}
+				}],
+			});
+			
 		}
 	});
 });
